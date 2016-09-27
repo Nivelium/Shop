25b2a55a6e4e6e9ff830e6c1fe911bcabe80b66f
Nivelium <McDeath@yandex.ru>
2016-09-28 00:39:08 +0300

    first file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const config = require('./config.json');
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({ port: config.port });
+server.path(__dirname + '/client');
+
+server.start((err) => {
+
+    if (err) {
+    throw err;
+}
+console.log(`Server running at: ${server.info.uri}`);
+});
